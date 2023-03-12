8a85948697f4681d46d20a1b5d9018ff5101206d
satiwari26 <tiwarisaumitra26@gmail.com>
2023-03-12 01:13:59 -0800

    new chnages made, filter works but still working on the magnitude of chuncks

--- finalLab/cpe367_dtmf_example2.py
+++ finalLab/cpe367_dtmf_example2.py
@@ -21,10 +21,14 @@ from my_fifo import my_fifo
 
 #global list to store the dft input signals
 #creating yout to store the new signal after running it through the filter
-yout1 = []
-yout2 = []
-yout3 = []
-yout4 = []
+peak_env697 = []
+peak_env770 = []
+peak_env852 = []
+peak_env1209 = []
+peak_env1336 = []
+peak_env1477 = []
+
+
 
 ############################################
 ############################################
@@ -56,63 +60,80 @@ def process_wav(fpath_sig_in):
 	sampleCount =0
 	ncountReset =0 # find the peak in 20 samples times this will give us the index of where it is happening in the s2.length
 
-	maxIndex1 =0
-	maxIndex2 =0
-	maxIndex3 = 0
-	maxIndex4 = 0
-	index1336 = 0
-	index1209= 0
-	index697 = 0
-	index770 = 0
+	for k in range(s2.get_len()):
+		peak_env697.append(0)
+		peak_env770.append(0)
+		peak_env852.append(0)
+		peak_env1209.append(0)
+		peak_env1336.append(0)
+		peak_env1477.append(0)
+
+	r1 = 0.9
+	b0 = 1-r1
+	#freq =0
+	a2 = r1*r1
+
+	fifo1209 = my_fifo(3)
+	fifo1336 = my_fifo(3)
+	fifo1477 = my_fifo(3)
+	fifo697 = my_fifo(3)
+	fifo770 = my_fifo(3)
+	fifo852 = my_fifo(3)
+
+	y1209 = []
+	y1336 = []
+	y1477 = []
+
+	y697 = []
+	y770 = []
+	y852 = []
+	for i in range(s2.get_len()):
+		y1209.append(0)
+		y1336.append(0)
+		y1477.append(0)
+
+		y697.append(0)
+		y770.append(0)
+		y852.append(0)
+
 	# process input	
 	xin = 0
 	for n_curr in range(s2.get_len()):
 		
 		# read next input sample from the signal analyzer
 		xin = s2.get('xin',n_curr)
-		#s2.set('sig_1',n_curr,xin)
+		s2.set('sig_1',n_curr,xin)
 		#symbol_val_det = 5
 
-		if (sampleCount ==9):
-			maxIndex1 = bandPassSig(bufferSamp,1209,yout1)
-			maxIndex2 = bandPassSig(bufferSamp,697,yout2) #call the filter function
-			maxIndex3 = bandPassSig(bufferSamp,1336,yout3)
-			maxIndex4 = bandPassSig(bufferSamp,770,yout4)
-			sampleCount=0
+		#8 different filter operations
+		y1209[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*1209/4000))*fifo1209.get(0) - a2*fifo1209.get(1)
+		y1336[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*1336/4000))*fifo1336.get(0) - a2*fifo1336.get(1)
+		y1477[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*1477/4000))*fifo1477.get(0) - a2*fifo1477.get(1)
 
-			#print('reset count '+str(ncountReset))
-			
-			index1209 = index1209+10+maxIndex1 
-			index697 = index697+10+maxIndex2
-			index1336 = index1336+10+maxIndex3
-			index770 = index770+10+maxIndex4
+		y697[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*697/4000))*fifo697.get(0) - a2*fifo697.get(1)
+		y770[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*770/4000))*fifo770.get(0) - a2*fifo770.get(1)
+		y852[n_curr] = b0*xin + (2*r1*np.cos(2*np.pi*852/4000))*fifo852.get(0) - a2*fifo852.get(1)
 
-			print(str(index1209)+ " index1209")
-			print(str(index697)+ " index697")
-			print(str(index1336)+ " index1336")
-		else:
-			sampleCount = sampleCount+1
-			bufferSamp[sampleCount] = xin
+		#updating all the filter operation
+		fifo1209.update(y1209[n_curr])
+		fifo1336.update(y1336[n_curr])
+		fifo1477.update(y1477[n_curr])
 
-		########################
-		# students: evaluate each filter and implement other processing blocks
+		fifo697.update(y697[n_curr])
+		fifo770.update(y770[n_curr])
+		fifo852.update(y852[n_curr])
 
-		#split it into different function to perform the buffere operation
-
-		#print(str(index1209)+ " index1209")
-		#print(str(index697)+ " index697")
-		#print(str(index1336)+ " index1336")
-
-
-		if(np.abs(index1209-index1336) <=5):
-			symbol_val_det = 2
-		if(np.abs(index1209-index697) <=5):
-			symbol_val_det  = 1
-		########################
-		# students: combine results from filtering stages
-		#  and find (best guess of) symbol that is present at this sample time
 		
-		
+		#peak_env697[n_curr] = np.max( np.abs(y697[]))/1024
+		peak_env(s2,y697)
+
+		s2.set('sig_1209',n_curr,y1209[n_curr])
+		s2.set('sig_1336',n_curr,y1336[n_curr])
+		s2.set('sig_1477',n_curr,y1477[n_curr])
+		s2.set('sig_697',n_curr,y697[n_curr])
+		s2.set('sig_770',n_curr,y770[n_curr])
+		s2.set('sig_852',n_curr,y852[n_curr])
+		s2.set('peak_envolp 697',n_curr,peak_env697[n_curr])
 
 
 		# save intermediate signals as needed, for plotting
@@ -123,7 +144,7 @@ def process_wav(fpath_sig_in):
 		#s2.set('sig_2',n_curr,2 * xin)
 
 		# save detected symbol
-		s2.set('symbol_det',n_curr,symbol_val_det)
+		#s2.set('symbol_det',n_curr,symbol_val_det)
 
 		# get correct symbol (provided within the signal analyzer)
 		#symbol_val = s2.get('symbol_val',n_curr)
@@ -135,101 +156,100 @@ def process_wav(fpath_sig_in):
 		# save error signal
 		#s2.set('error',n_curr,symbol_val_err)
 
+	#dftMag(y852,s2)	
 	
 	#absolute value of the dft
 
-	for t in range(s2.get_len()):
+	#for t in range(s2.get_len()):
 		#plotting mag of dft after filtering it
-		s2.set('sig_2',t,yout1[t])
+		#s2.set('sig_2',t,envelopeSig[t])
 
-		s2.set('sig_1',t,yout3[t])
+		#s2.set('sig_1209',t,yout2[t])
 
-		symbol_val_det = 0
+		#symbol_val_det = 0
 
 		# save detected symbol
 		#s2.set('symbol_det',t,symbol_val_det)
 
 		# get correct symbol (provided within the signal analyzer)
-		symbol_val = s2.get('symbol_val',t)
+		#symbol_val = s2.get('symbol_val',t)
 
 		# compare detected signal to correct signal
-		symbol_val_err = 0
-		if symbol_val != symbol_val_det: symbol_val_err = 1
+		#symbol_val_err = 0
+		#if symbol_val != symbol_val_det: symbol_val_err = 1
 		
 		# save error signal
-		s2.set('error',t,symbol_val_err)
+		#s2.set('error',t,symbol_val_err)
 	
 	# display mean of error signal
-	err_mean = s2.get_mean('error')
-	print('mean error = '+str( round(100 * err_mean,1) )+'%')
+	#err_mean = s2.get_mean('error')
+	#print('mean error = '+str( round(100 * err_mean,1) )+'%')
 		
 	# define which signals should be plotted
-	plot_sig_list = ['sig_1','sig_2','symbol_val','symbol_det','error']
+	#plot_sig_list = ['sig_1','sig_2','symbol_val','symbol_det','error']
+	plot_sig_list = ['sig_1209','sig_1336','sig_1477','sig_697','sig_770','sig_852','peak_envolp 697']
 	
 	# plot results
 	s2.plot(plot_sig_list)
 	
 	return True
 
-#filter operation on the signal with certain buffer length
-def bandPassSig(bufferSample,freq,youtG):
-	#fifo class that will have the same length
-	maxindex =0
-	fifo1 = my_fifo(3)
-	
-	#initialize buff to store the ybuff_out
-	yBuff = []
-	for s in range(10):
-		yBuff.append(0)
 
-	# students: setup filters
-	r1 = 0.9
-	b0 = 1-r1
-	#freq =0
-	a2 = r1*r1
 
-	#apply filter and update the samples
-	for i in range(10):
-		yBuff[i] = b0*bufferSample[i] + (-2*r1*np.cos(2*np.pi*freq/4000))*fifo1.get(0) + a2*fifo1.get(1)
-		fifo1.update(yBuff[i])
-	maxIndex = dftMag(yBuff,youtG)
+def peak_env(s2,y697):
+	#absolute = np.abs(y697)
 
-	return maxIndex
+	for i in range(s2.get_len()-1024):
+		maxVal = np.max( np.abs(y697[i:i+1024]))/1024
 
+		for k in range(i,i+1024):
+			peak_env697[k] = maxVal
+		i = i+1024
 
 
 #dft of the signal after applying filter to it
-def dftMag(yout,youtG):
+def dftMag(yout,s2):
 	xk = []
-	maxVal = 0
-	maxIndex =0
-	for s in range(10):
+	for s in range(s2.get_len()):
 		xk.append(0.0)
 	
-	for l in range(10):
-		for k in range(10):
-			xk[l] = xk[l] + yout[k]*(np.cos((2*np.pi*l*k/(20)) - (1j*np.sin(2*np.pi*l*k/20))))
+	for l in range(s2.get_len()):
+		for k in range(s2.get_len()):
+			xk[l] = xk[l] + yout[k]*(np.cos((2*np.pi*l*k/(2*s2.get_len())) - (1j*np.sin(2*np.pi*l*k/(2*s2.get_len())))))
 
 	#absolute value of the dft
 	dftMag1 = np.abs(xk)
+	#dftMag2 =[]
+	#for s in range(2000):
+	#	dftMag2.append(0)
 
-	#global val storer func
-	globalValStorer(dftMag1,youtG)
+	x_comp = []
+	for t in range(s2.get_len()):
+		#print(t)
+		x_comp.append(t)
 
-	for t in range(len(dftMag1)): #find the max peak and its index
-		if(dftMag1[t]>maxVal):
-			maxVal = dftMag1[t]
-			maxIndex = t
-			#print(str(maxVal)+' '+str(maxIndex))
-	#print('returning max index' + str(maxIndex))
-	return maxIndex
+	#for k in range(2000):
+	#	dftMag2[k] = dftMag1[k]
 
+	exact_freq = 0
+	freq_mag = 0
+	mag = 0
+
+	for p in range(s2.get_len()):
+		freq_mag = freq_mag+(p*dftMag1[p])
+		mag = mag+(dftMag1[p])
 	
+	exact_freq = freq_mag/mag
 
-def globalValStorer(dftMag1,yout):
-	#store it in the global variable
-	for t in range(10):
-		yout.append(dftMag1[t])
+	print("weighted frequency is: " + str(exact_freq))
+
+	fig, ax = plt.subplots() 
+	ax.plot(x_comp, dftMag1) 
+	ax.set(xlabel='Frequency (Hz)', ylabel='Magnitude', title='tile 2E ') 
+	ax.grid() 
+ 
+	fig.savefig('image_file.png') 
+	plt.show() 
 	
 	
 	
--- finalLab/fifoTest.py
+++ finalLab/fifoTest.py
@@ -1,11 +1,23 @@
-from my_fifo import my_fifo
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import lfilter
 
-fifo = my_fifo(3)
-x= [0]
-for i in range(len(x)-1,len(x)+10):
-    x.append(i)
+# Define filter coefficients
+r1 = 0.9
+b0 = [1, -r1, 0]
+a = [1, -2*r1*np.cos(2*np.pi*697/4000), r1*r1]
 
-print(x[0])
-print(x[1])
-print(x[2])
-print(x[3])
+# Generate input signal
+n = np.arange(0, 1000)
+xin = np.sin(2*np.pi*1000/4000*n) + np.sin(2*np.pi*2000/4000*n)
+
+# Apply filter using lfilter function
+yout = lfilter(b0, a, xin)
+
+# Plot input and output signals
+plt.plot(n, xin, label='Input')
+plt.plot(n, yout, label='Output')
+plt.legend()
+plt.xlabel('Sample')
+plt.ylabel('Amplitude')
+plt.show()
